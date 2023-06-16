361c5aa345dc3a0d022d22c0cc741283d93c50ea
egwilborn <egwilborn@gmail.com>
2023-06-16 14:48:52 -0500

    add word array js file

---
+++ js/app.js
@@ -0,0 +1,14 @@
+  /*----- constants -----*/
+const wordBank = ["advise", "allege", "beyond","choice", "danger", "equity", "fought", "gangly", "hurled", "infant","jingle", "kinder"]
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
---
+++ js/word-array.js
@@ -0,0 +1,1 @@
+const wordBank = ["advise", "allege", "beyond"]
