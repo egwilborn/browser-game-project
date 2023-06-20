55be6d111bef6f2a26ea9714c32a0ba37ca94192
egwilborn <egwilborn@gmail.com>
2023-06-20 13:00:55 -0500

    renders guesses

--- js/app.js
+++ js/app.js
@@ -3,34 +3,37 @@
 
   /*----- state variables -----*/
 let guessWord; //will decide which word will be guessed by player (Form: string)
-let letterGuess; // will show whether the guessed letter is in the word or not
+let guessStatus; // will show whether the guessed letter is in the word or not
 //   True or false (true - the letter is in the word; false - the letter is not in the word; null when there is no guess yet)
 let GameOutcome; //will decide whether the game is in play, player won, or player lost
 //   Currently playing: null
 //   Player won: 1
 //   Player lost: -1
   let petals; //shows how many petals are on the flower and how many incorrect guesses the player has left
-
-  let gameStatus = "start";  //shows where in the game you are values are "start" "playing" or "finished"
+  let guessedLetter; // pulls the letter from the button that was clicked to compare to the guess word
+  let playerString; //this is an array containing all the letters that have been guessed in the correct order
+  let gameStatus = "start";  //shows where in the game you are values are "start" "playing" "won" or "lost"
 
   /*----- cached elements  -----*/
 const gameStartBtn = document.getElementById("start-game");
 const startOverBtn = document.getElementById("start-over");
 const messageEl = document.getElementById("message");
-const letterBtns = document.querySelectorAll("#letter-buttons > button");
+const letterBtns = [...document.querySelectorAll("#letter-buttons > button")];
 const flowerPetalEls =[...document.querySelectorAll("#flower > div")]
 const guessWordEls = [...document.querySelectorAll("#guess-word > div")]
   /*----- event listeners -----*/
 gameStartBtn.addEventListener("click", init)
-document.getElementById("letter-buttons").addEventListener("click", handleGuess);// ??? maybe need to just use variable
-
+document.getElementById("letter-buttons").addEventListener("click", handleGuess);
+startOverBtn.addEventListener("click", handleRestart);
   /*----- functions -----*/
   //when the play game button is clicked, the game will be initialized
 function init() {
     guessWord = wordBank[Math.floor(Math.random()*wordBank.length)];
-    letterGuess = null;
+    guessStatus = null;
     gameOutcome = null;
+    gameStatus = "playing";
     petals = 8;
+    guessString = new Array(guessWord.length);
     render();
 }
 
@@ -39,36 +42,74 @@ function render() {
     renderButtons();
     renderGuessOutcome();
 }
+
 function renderMessage() {
+  if (gameStatus === "playing") {
     messageEl.innerText = "";
+}else if (gameStatus === "won") {
+  messageEl.innerText = "Congratulats! You guessed the word!"
+  messageEl.style.backgroundColor = "blue"
+} else if (gameStatus === "lost") {
+  messageEl.innerText = `Oops! The word was ${guessWord}.`
+} else if (gameStatus === "start") {
+  messageEl.innerHTML = "<h4>Guess each letter of the word</h4> <h4>Can you keep the flower from losing its petals?</h4>"
+  messageEl.style.backgroundColor = "rgb(182 230 238)"
+}
 }
 
 function renderButtons() {
+  if (gameStatus === "playing") {
     startOverBtn.style.visibility = "visible";
     gameStartBtn.style.visibility = "hidden";
+  } else if (gameStatus === "won" || gameStatus === "lost") {
+    startOverBtn.innerText = "Play Again?"
+  } else if (gameStatus === "start") {
+    startOverBtn.style.visibility = "hidden";
+    gameStartBtn.style.visibility = "visible";
+    document.querySelectorAll("#guessWord > div").innerText = "__";
+    letterBtns.forEach(function(Btn) {
+      Btn.style.backgroundColor = "rgb(107, 210, 172)";
+      Btn.disabled = false;
+    })
+  
+  }
 }
 
 function renderGuessOutcome() {
-if (letterGuess === true) {
-  //need to check where in the word the letter lies
-  const splitWord = guessWord.split("")
-  splitWord.forEach(function(letter) {
-    if (letter === guess) {
-      
+if (guessStatus === true) {
+  //need to render the guessString visually
+  guessString.forEach(function(letter, idx) {
+    if (letter === guessedLetter.toUpperCase()) {
+      guessWordEls[idx].innerText = guessedLetter.toUpperCase();
     }
   })
-  } else if (letterGuess === false) {
+  //now I need to show that the correct button has been used by making any correct guess buttons pink
+  const guessBtn = letterBtns.find(function(Btn) {
+  return Btn.innerText === guessedLetter.toUpperCase();
+})
+  guessBtn.style.backgroundColor = "rgb(255 188 193)";
+ 
+  } else if (guessStatus === false) {
+
     //flower div needs to lose a petal in the state variable and in the html
    flowerPetalEls[petals].remove();
-  } else {
-    return;
-  }
+   //incorrect guess buttons should be grey and the letter should be struck through
+   const guessBtn = letterBtns.find(function(Btn) {
+    return Btn.innerText === guessedLetter.toUpperCase();
+  })
+    guessBtn.style.backgroundColor = "grey";
+    guessBtn.style.textDecorationLine = "line-through";
+    guessBtn.disabled = true;
+
+  } 
 }
+
+
 
 
 function handleGuess(evt) {
   //when a letter button is clicked, the text is pulled from the target
-  let guessedLetter = evt.target.innerText.toLowerCase();
+  guessedLetter = evt.target.innerText.toLowerCase();
   //set up guards against clicking something other than a button
   if(guessedLetter.length > 1) {
     return;
@@ -77,23 +118,36 @@ function handleGuess(evt) {
     if (guessWord === undefined) {
       return;
     } else if (guessWord.includes(guessedLetter)) {
-    letterGuess = true;
+    guessStatus = true;
+    const splitWord = guessWord.split("")
+    splitWord.forEach(function(letter, idx) {
+      if (letter === guessedLetter) {
+      guessString[idx] = `${letter.toUpperCase()}`;
+      }
+    })
     } else {
-      letterGuess = false;
+      guessStatus = false;
       petals = petals-1;
     }
-
   }
+  render();
   checkGameOutcome();
+
+  console.log(gameStatus);
+}
+
+function checkGameOutcome() {
+//checks if the game is over
+if (guessString.join("").toLowerCase() === guessWord) {
+ return gameStatus = "won";
+} else if (petals === 0) {
+ return gameStatus = "lost";
+}
+render();
+}
+
+function handleRestart() {
+  gameStatus = "start";
   render();
 }
-function checkGameOutcome() {
 
-}
-
-
-//questions to answer
-//showed letterGuess be a boolean or should it instead be the letter that was guessed and all conditionals are just compared to guessword?
-//current plan for rendering true guess - split guess word and operate over it to check if each letter is the same as the guess letter. if it is, print the index, if not, don't do anything
-//with the index, update the corresponding place in the guess word divs with the guess letter text
-//do we need a game status state variable for the message and the button rendering???
